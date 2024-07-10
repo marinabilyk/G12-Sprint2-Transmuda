86495bb6d3e26dad5255ee24ea60ab832a363758
sevara <sevara1206@outlook.com>
2024-07-10 13:42:12 -0400

    created Create Calendar Events page and added some locators

---
+++ src/test/java/com/transmuda/pages/CreateCalendarEventPage.java
@@ -0,0 +1,22 @@
+package com.transmuda.pages;
+
+import com.transmuda.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CreateCalendarEventPage {
+
+    public CreateCalendarEventPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//input[contains(@id,'recurrence-repeat-view')]")
+    public WebElement repeat_button;
+
+    @FindBy(xpath = "/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/form/div[2]/div[2]/div/div[1]/div[2]/div/fieldset/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/span/span/span")
+    public WebElement error_message;
+
+    @FindBy(xpath = "(//input[contains(@data-related-field,'interval')])[1]")
+    public WebElement repeatEveryButton;
+
+}
