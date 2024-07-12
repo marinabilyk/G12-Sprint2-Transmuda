ed69cc387e455819e13b3e81abcaa21586e97e6a
sevara <sevara1206@outlook.com>
2024-07-12 14:27:03 -0400

    created US07 feature and Step Defs

---
+++ src/test/java/com/transmuda/step_definitions/US07CloneStepDefs.java
@@ -0,0 +1,36 @@
+package com.transmuda.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US07CloneStepDefs {
+    @When("User clicks Fleet tab")
+    public void user_clicks_fleet_tab() {
+
+    }
+
+    @When("User chooses Vehicles link")
+    public void user_chooses_vehicles_link() {
+
+    }
+
+    @When("User sees all unchecked checkboxes")
+    public void user_sees_all_unchecked_checkboxes() {
+
+    }
+
+    @When("User clicks on first checkbox to check all the cars")
+    public void user_clicks_on_first_checkbox_to_check_all_the_cars() {
+
+    }
+
+    @When("User unchecks first checkbox")
+    public void user_unchecks_first_checkbox() {
+
+    }
+
+    @Then("User selects a random car")
+    public void user_selects_a_random_car() {
+
+    }
+}
