206ad924f9a3cd9f7dd0714a634a45c4a8ab94fc
Katthaleeya <katthaleeya05@gmail.com>
2024-07-12 11:23:34 -0400

    added some changes on BasePage

---
+++ src/test/java/com/transmuda/pages/AccountsPage.java
@@ -0,0 +1,33 @@
+package com.transmuda.pages;
+
+import com.transmuda.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountsPage extends BasePage{
+
+    public AccountsPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//div[@class='filter-container']/div/ul/li/label/span")
+    public List<WebElement> filterNames;
+
+    @FindBy(xpath = "//div[@class='filter-container']/button")
+    public WebElement manageFilters;
+
+    @FindBy(xpath = "//select[@data-action='add-filter-select']")
+    public WebElement dropdown;
+
+    
+    }
+
+
+
