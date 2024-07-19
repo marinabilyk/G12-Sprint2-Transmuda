ee9a25be4b29d54ff02229b956300c31b933e257
Katthaleeya <katthaleeya05@gmail.com>
2024-07-19 15:49:28 -0400

    made some changes on VehiclePage

--- src/test/java/com/transmuda/pages/VehiclesPage.java
+++ src/test/java/com/transmuda/pages/VehiclesPage.java
@@ -7,12 +7,13 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.security.PublicKey;
+import java.util.List;
 import java.util.Set;
 
 public class VehiclesPage {
 
-    public VehiclesPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public VehiclesPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(xpath = "(//button[@class='btn btn-default btn-small dropdown-toggle']/input[@type='checkbox'])[1]")
@@ -24,4 +25,17 @@ public class VehiclesPage {
     @FindBy(xpath = "//tbody[@class='grid-body']//tr")
     public Set<WebElement> gridBody;
 
+    @FindBy(xpath = "(//ul[@class='dropdown-menu dropdown-menu__action-cell launchers-dropdown-menu'])[1]")
+    public WebElement editInfoIcons;
+
+
+
+
+    @FindBy (xpath = "(//ul[@class='dropdown-menu dropdown-menu__action-cell launchers-dropdown-menu'])[1]")
+    public WebElement dropDownIcons;
+
+
+
+
 }
+
