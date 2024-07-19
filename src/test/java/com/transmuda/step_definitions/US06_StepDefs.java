8cefdda4a45f45924db53b171401426ad6d4b6fa
Katthaleeya <katthaleeya05@gmail.com>
2024-07-19 15:50:56 -0400

    Created US06 StepDefs

---
+++ src/test/java/com/transmuda/step_definitions/US06_StepDefs.java
@@ -0,0 +1,66 @@
+package com.transmuda.step_definitions;
+
+import com.transmuda.pages.BasePage;
+import com.transmuda.pages.VehiclesPage;
+import com.transmuda.utilities.BrowserUtils;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+
+import java.util.*;
+
+public class US06_StepDefs extends BasePage {
+
+    VehiclesPage vehiclesPage = new VehiclesPage();
+
+
+    @When("the user selects on Vehicles")
+    public void the_user_selects_on_vehicles() {
+
+
+        BrowserUtils.waitFor(2);
+
+        vehiclesButton.click();
+
+
+    }
+
+
+    @And("user hovers the mouse over edit icons")
+    public void userHoversTheMouseOverEditIcons() {
+
+        BrowserUtils.hover(vehiclesPage.editInfoIcons);
+
+        BrowserUtils.waitFor(2);
+
+        String actualDots = vehiclesPage.editInfoIcons.getAttribute("class");
+
+        String expectedDots = "dropdown-menu dropdown-menu__action-cell launchers-dropdown-menu";
+
+        Assert.assertEquals(actualDots, expectedDots);
+
+    }
+
+
+    @Then("user sees edit info icons")
+    public void userSeesEditInfoIcons() {
+
+        String actualIcons = vehiclesPage.dropDownIcons.getAttribute("class");
+
+        String expectedIcons = "dropdown-menu dropdown-menu__action-cell launchers-dropdown-menu";
+
+        Assert.assertEquals(actualIcons,expectedIcons);
+
+    }
+}
+
+
+
+
+
+
+
