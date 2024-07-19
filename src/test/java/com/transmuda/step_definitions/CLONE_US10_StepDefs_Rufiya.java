6afc84eb8959c4fd8a38994e05c268aeb7cec4cf
“Rufa2008” <rufa1989@gmail.com>
2024-07-18 23:34:27 -0400

    US10_Clone completed Add static method to BrowserUtils

---
+++ src/test/java/com/transmuda/step_definitions/CLONE_US10_StepDefs_Rufiya.java
@@ -0,0 +1,37 @@
+package com.transmuda.step_definitions;
+
+import com.transmuda.pages.CalendarEventsPage;
+import com.transmuda.utilities.BrowserUtils;
+import com.transmuda.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.devtools.v118.input.model.DragDataItem;
+
+public class CLONE_US10_StepDefs_Rufiya {
+    CalendarEventsPage calendarEventsPage=new CalendarEventsPage();
+
+    @When("user navigate to Calendar event page")
+    public void user_navigate_to_calendar_event_page() {
+
+        calendarEventsPage.waitUntilLoaderScreenDisappear();
+        calendarEventsPage.navigateToModule("Activities","Calendar Events");
+    }
+
+    @When("user navigate to Create Calendar Event page")
+    public void user_navigate_to_create_calendar_event_page() {
+
+        BrowserUtils.waitForVisibility(calendarEventsPage.linkCreateCalendarEvent, 5);
+        calendarEventsPage.linkCreateCalendarEvent.click();
+    }
+
+    @Then("user should be able to type {string} into Description field")
+    public void user_should_be_able_to_type_into_description_field(String message) {
+
+        Driver.getDriver().switchTo().frame(calendarEventsPage.iframeDescription);
+        calendarEventsPage.descriptionInput.sendKeys(message);
+
+        Assert.assertEquals(calendarEventsPage.descriptionInput.getText(), message);
+
+    }
+}
